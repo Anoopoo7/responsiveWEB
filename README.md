1626fa9835de9ddd6a41ab1ee8a406d40228cc85
Anoopoo7 <anoopviswanadh007@gmail.com>
2020-12-18 22:42:44 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # responsiveWEB
+# responsiveWEB
---
+++ jsfile.js
@@ -0,0 +1,19 @@
+document.querySelector('#button').addEventListener("click",function(){
+  const name = document.querySelector('#input').value;
+  alert(`Sorry, ${name} is not available now`)
+});
+document.querySelector('.demo1').addEventListener("click",function() {
+  alert("Sorry, this option is under construction.")
+});
+document.querySelector('.demo2').addEventListener("click",function() {
+  alert("Sorry, this option is under construction.")
+});
+document.querySelector('.demo3').addEventListener("click",function() {
+  alert("Sorry, this option is under construction.")
+});
+document.querySelector('.demo4').addEventListener("click",function() {
+  alert("Sorry, this option is under construction.")
+});
+document.querySelector('.demo5').addEventListener("click",function() {
+  alert("Sorry, this option is under construction.")
+});
